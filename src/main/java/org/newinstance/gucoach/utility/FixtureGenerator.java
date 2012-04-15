3ebd389f0e861467410a4513ba69a9ca222f0adf
Marc Walter <m.walter@ymail.com>
2012-04-15 02:07:23 +0200

    added generator to generate fixtures

---
+++ src/main/java/org/newinstance/gucoach/utility/FixtureGenerator.java
@@ -0,0 +1,96 @@
+/*
+ * GUCoach - your personal coach for Goalunited (tm).
+ * Copyright (C) 2012 newInstance.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.newinstance.gucoach.utility;
+
+import java.util.Calendar;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Generates all fixtures for a season.
+ *
+ * @author mwalter
+ */
+public final class FixtureGenerator {
+
+    /** Every league consists of 12 teams. */
+    private static final int NUMBER_OF_TEAMS_IN_LEAGUE = 12;
+
+    /** Holds all fixtures. First dimension is the matchday, second dimension is the group of 6 fixtures and third dimension is the fixture. */
+    // TODO should be computed if possible
+    private static final int[][][] FIXTURES = {{{1, 11}, {2, 9}, {3, 8}, {4, 7}, {10, 6}, {12, 5}}, {{1, 4}, {6, 12}, {7, 10}, {8, 5}, {9, 3}, {11, 2}},
+            {{2, 1}, {3, 11}, {4, 9}, {5, 6}, {10, 8}, {12, 7}}, {{1, 12}, {2, 3}, {7, 6}, {8, 4}, {9, 10}, {11, 5}},
+            {{3, 1}, {4, 2}, {5, 7}, {6, 8}, {10, 11}, {12, 9}}, {{1, 5}, {2, 10}, {3, 4}, {8, 7}, {9, 6}, {11, 12}},
+            {{4, 11}, {5, 2}, {6, 1}, {7, 9}, {10, 3}, {12, 8}}, {{1, 7}, {2, 12}, {3, 5}, {4, 10}, {9, 8}, {11, 6}},
+            {{5, 9}, {6, 3}, {7, 2}, {8, 11}, {10, 1}, {12, 4}}, {{1, 9}, {2, 8}, {3, 12}, {4, 6}, {10, 5}, {11, 7}},
+            {{5, 4}, {6, 2}, {7, 3}, {8, 1}, {9, 11}, {12, 10}}, {{11, 1}, {9, 2}, {8, 3}, {7, 4}, {6, 10}, {5, 12}},
+            {{4, 1}, {12, 6}, {10, 7}, {5, 8}, {3, 9}, {2, 11}}, {{1, 2}, {11, 3}, {9, 4}, {6, 5}, {8, 10}, {7, 12}},
+            {{12, 1}, {3, 2}, {6, 7}, {4, 8}, {10, 9}, {5, 11}}, {{1, 3}, {2, 4}, {7, 5}, {8, 6}, {11, 10}, {9, 12}},
+            {{5, 1}, {10, 2}, {4, 3}, {7, 8}, {6, 9}, {12, 11}}, {{11, 4}, {2, 5}, {1, 6}, {9, 7}, {3, 10}, {8, 12}},
+            {{7, 1}, {12, 2}, {5, 3}, {10, 4}, {8, 9}, {6, 11}}, {{9, 5}, {3, 6}, {2, 7}, {11, 8}, {1, 10}, {4, 12}},
+            {{9, 1}, {8, 2}, {12, 3}, {6, 4}, {5, 10}, {7, 11}}, {{4, 5}, {2, 6}, {3, 7}, {1, 8}, {11, 9}, {10, 12}}};
+
+    /**
+     * Generated the fixtures for all teams playing in a league based on the fixtures table.
+     *
+     * @param teamIds the ids of the teams playing in the league
+     * @param firstMatchday the first matchday of the season, needed to calculate all following matchdays
+     * @return a map containing all fixtures ordered by matchday
+     */
+    public static Map<Calendar, Map<Integer, Long[]>> generateFixtures(final List<Long> teamIds, final Calendar firstMatchday) {
+        // TODO can be removed if GUI checks for 12 teams being entered by user
+        if (teamIds.size() != NUMBER_OF_TEAMS_IN_LEAGUE) {
+            throw new IllegalArgumentException("Illegal number of teams.");
+        }
+
+        final Long[] teams = teamIds.toArray(new Long[teamIds.size()]);
+        // do not change original calendar so make a copy
+        final Calendar matchday = (Calendar) firstMatchday.clone();
+        final Map<Calendar, Map<Integer, Long[]>> fixtures = new LinkedHashMap<Calendar, Map<Integer, Long[]>>();
+
+        // iterate over matchdays
+        for (int day = 0; day < FIXTURES.length; day++) {
+            final Map<Integer, Long[]> fixturesOfMatchday = new LinkedHashMap<Integer, Long[]>();
+            // iterate over fixtures of a matchday
+            for (int fixture = 0; fixture < FIXTURES[day].length; fixture++) {
+                // get home team id
+                final Long homeTeamId = teams[FIXTURES[day][fixture][0] - 1];
+                // get away team id
+                final Long awayTeamId = teams[FIXTURES[day][fixture][1] - 1];
+                fixturesOfMatchday.put(fixture, new Long[] {homeTeamId, awayTeamId});
+            }
+            // put all fixtures of one matchday into calendar map
+            fixtures.put(matchday, fixturesOfMatchday);
+            // and increase date
+            if (day % 2 == 0) {
+                // even - next matchday will be in 4 days
+                matchday.add(Calendar.DAY_OF_MONTH, 4);
+            } else {
+                // odd - next matchday will be in 3 days
+                matchday.add(Calendar.DAY_OF_MONTH, 3);
+            }
+        }
+        return fixtures;
+    }
+
+    private FixtureGenerator() {
+        // hide constructor
+    }
+}
---
+++ src/test/java/org/newinstance/gucoach/utility/FixtureGeneratorTest.java
@@ -0,0 +1,83 @@
+/*
+ * GUCoach - your personal coach for Goalunited (tm).
+ * Copyright (C) 2012 newInstance.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.newinstance.gucoach.utility;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Tests the methods of the {@link FixtureGenerator}.
+ *
+ * @author mwalter
+ */
+public class FixtureGeneratorTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void checkGenerateFixturesWithInsufficientTeams() {
+        final List<Long> teamIds = new ArrayList<Long>();
+        teamIds.add(1L);
+        FixtureGenerator.generateFixtures(teamIds, Calendar.getInstance());
+    }
+
+    @Test
+    public void checkGenerateFixtures() {
+        final Map<Calendar, Map<Integer, Long[]>> fixtures = FixtureGenerator.generateFixtures(getTeamIds(), Calendar.getInstance());
+        for (Map<Integer, Long[]> fixturesOfMatchday : fixtures.values()) {
+            // there have to be allways 6 fixtures per matchday
+            Assert.assertEquals(6, fixturesOfMatchday.size());
+
+            final List<Long> allTeams = getTeamIds();
+            for (Long[] fixture : fixturesOfMatchday.values()) {
+                Assert.assertTrue(allTeams.contains(fixture[0]));
+                Assert.assertTrue(allTeams.contains(fixture[1]));
+                allTeams.remove(fixture[0]);
+                allTeams.remove(fixture[1]);
+            }
+            // if list is empty all teams had been lined up for a fixture
+            Assert.assertTrue(allTeams.isEmpty());
+        }
+    }
+
+    /**
+     * Returns a list with some team ids.
+     *
+     * @return a list with team ids
+     */
+    private List<Long> getTeamIds() {
+        final List<Long> teamIds = new ArrayList<Long>();
+        teamIds.add(1L);
+        teamIds.add(2L);
+        teamIds.add(3L);
+        teamIds.add(4L);
+        teamIds.add(5L);
+        teamIds.add(6L);
+        teamIds.add(7L);
+        teamIds.add(8L);
+        teamIds.add(9L);
+        teamIds.add(10L);
+        teamIds.add(11L);
+        teamIds.add(12L);
+        return teamIds;
+    }
+}
