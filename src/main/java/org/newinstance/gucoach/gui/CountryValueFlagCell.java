8bb9be23e247fdb695c4989df02ff647c9ba62f4
Marc Walter <m.walter@ymail.com>
2012-04-16 00:45:06 +0200

    uses MainController

--- src/main/java/org/newinstance/gucoach/gui/CountryValueFlagCell.java
+++ src/main/java/org/newinstance/gucoach/gui/CountryValueFlagCell.java
@@ -22,6 +22,7 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import org.newinstance.gucoach.gui.controller.MainController;
 import org.newinstance.gucoach.model.Country;
 
 /**
