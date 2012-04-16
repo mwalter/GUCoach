1dd98ed8a2c892e5ff8865a66446752dbc5fb4df
Marc Walter <m.walter@ymail.com>
2012-04-16 23:31:44 +0200

    added initial version of create league dialogue

--- src/main/java/org/newinstance/gucoach/gui/controller/LeagueController.java
+++ src/main/java/org/newinstance/gucoach/gui/controller/LeagueController.java
@@ -19,6 +19,15 @@
 package org.newinstance.gucoach.gui.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ResourceBundle;
 
 /**
  * TODO document me
@@ -27,12 +36,41 @@ import javafx.event.ActionEvent;
  */
 public class LeagueController {
 
-    public void createLeague(final ActionEvent event) {
+    /** The window title. */
+    public static final String TITLE_CREATE_LEAGUE = "Create League";
+    /** The layout of the main application window. */
+    public static final String SCENE_CREATE_LEAGUE = "../fxml/windowCreateLeague.fxml";
 
+    @FXML
+    private BorderPane borderPane;
+
+    public void showCreateLeagueWindow(final ActionEvent event) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource(SCENE_CREATE_LEAGUE), ResourceBundle.getBundle("ApplicationResources"));
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+
+        final Scene scene = new Scene(root);
+        final Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle(TITLE_CREATE_LEAGUE);
+        stage.show();
     }
 
     public void editFixture(final ActionEvent event) {
 
     }
 
+    /**
+     * Cancels the create league process and closes the window.
+     *
+     * @param event the event
+     */
+    public void closeWindow(final ActionEvent event) {
+        final Stage stage = (Stage) borderPane.getScene().getWindow();
+        stage.close();
+    }
 }
