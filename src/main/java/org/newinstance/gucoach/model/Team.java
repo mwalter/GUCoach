2b803f3e567a51b588922a5d8b0a1984b908c057
mwalter <marc.walter.ch@gmail.com>
2013-02-19 21:46:26 +0100

    added named query

--- src/main/java/org/newinstance/gucoach/model/Team.java
+++ src/main/java/org/newinstance/gucoach/model/Team.java
@@ -24,6 +24,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
@@ -33,6 +34,7 @@ import javax.persistence.UniqueConstraint;
  * @author mwalter
  */
 @Entity
+@NamedQuery(name = "FIND_ALL_TEAM", query = "SELECT t FROM Team t")
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = {"name"}))
 public final class Team {
 
