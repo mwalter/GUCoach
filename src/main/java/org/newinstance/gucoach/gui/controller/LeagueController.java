13d9e9dfce104d53aaed61c3550f683665981a39
mwalter <marc.walter.ch@gmail.com>
2013-03-18 20:44:05 +0100

    removed unnecessary methods

--- src/main/java/org/newinstance/gucoach/gui/controller/LeagueController.java
+++ src/main/java/org/newinstance/gucoach/gui/controller/LeagueController.java
@@ -22,15 +22,9 @@ package org.newinstance.gucoach.gui.controller;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.TableView;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 import org.newinstance.gucoach.gui.StandingsContentProvider;
 import org.newinstance.gucoach.gui.StandingsDataRow;
-import org.newinstance.gucoach.gui.builder.CreateLeagueSceneBuilder;
-import org.newinstance.gucoach.utility.ResourceLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -55,63 +49,7 @@ public class LeagueController {
     @FXML
     private TableView tableViewStandings;
 
-//    @Override
-//    @SuppressWarnings("unchecked")
-//    public void initialize(final URL location, final ResourceBundle resources) {
-//        // set controller into provider
-//        // ControllerProvider.getInstance().setLeagueController(this);
-//
-//        // load standings data and fill standings table
-//        standingsData = standingsContentProvider.getStandingsData();
-//        tableViewStandings.setItems(standingsData);
-//
-//        // if there are no standings yet display create league message
-//        if (standingsData.isEmpty()) {
-//            final Label message = new Label();
-//            message.setText(ResourceLoader.getMessage(MessageId.I002.getMessageKey()));
-//            tableViewStandings.setPlaceholder(message);
-//        }
-//
-//        // add listener for standings list changes
-//        standingsData.addListener(new ListChangeListener<StandingsDataRow>() {
-//
-//            @Override
-//            public void onChanged(final Change<? extends StandingsDataRow> change) {
-//                tableViewStandings.setItems(change.getList());
-//            }
-//        });
-//
-//    }
-
-    /**
-     * Builds and shows the dialogue to create a new league.
-     *
-     * @param event the action event
-     */
-    public void showCreateLeagueWindow(final ActionEvent event) {
-        // build dialogue with builder
-        final Parent root = new CreateLeagueSceneBuilder().buildScene();
-
-        // build dialogue with FXML
-        //        Parent root = null;
-        //        try {
-        //            root = FXMLLoader.load(getClass().getResource(SCENE_CREATE_LEAGUE), ResourceBundle.getBundle("ApplicationResources"));
-        //        } catch (final IOException e) {
-        //            e.printStackTrace();
-        //        }
-
-        final Scene scene = new Scene(root);
-        scene.getStylesheets().add("gucoach.css");
-        final Stage stage = new Stage();
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.setTitle(ResourceLoader.getResource(TITLE_CREATE_LEAGUE));
-        stage.show();
-    }
-
     public void editFixture(final ActionEvent event) {
-
     }
 
 }
